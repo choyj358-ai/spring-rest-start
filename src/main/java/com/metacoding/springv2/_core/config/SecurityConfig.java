914aa653a01645c112a4e9037e826157b994848b
yujicoding <choyj358@gmail.com>
2026-02-19 12:56:58 +0900

    세션방식 로그인

--- src/main/java/com/metacoding/springv2/_core/config/SecurityConfig.java
+++ src/main/java/com/metacoding/springv2/_core/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.metacoding.springv2._core.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -10,6 +11,11 @@ import com.metacoding.springv2._core.filter.JwtAuthorizationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder encode() {
+        return new BCryptPasswordEncoder();
+    }
 
     // 시큐리티 필터 등록
     @Bean
@@ -22,11 +28,15 @@ public class SecurityConfig {
                 .anyRequest()
                 .permitAll()); // api가 붙은 주소는 다 통과
 
+        // UsernamePasswordAuthenticationFilter
         // 폼 로그인 비활성화(POST : x-www-form-urlencoded : username, password)
         http.formLogin(f -> f.disable());
 
         // 베이직 인증 활성화 시킴 (request 할 때 마다 username, password를 요구) -> 비활성화
         http.httpBasic(b -> b.disable());
+
+        // input에 crsf 토큰 받는 것을 비활성화하기
+        http.csrf(c -> c.disable());
 
         // 인증 필터를 변경
         http.addFilterBefore(new JwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
